4829c5a17be2c67ad204fe67bc1efd2496a88bb2
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-01-06 00:12:50 +0530

    navbar edge rounded

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./style.css";
 
 export default function index() {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-white rounded-pill">
+    <div class="pt-0">
+      <nav class="navbar navbar-expand-lg navbar-light bg-white rounded-bottom">
         <div class="container-fluid">
           <button
             class="navbar-toggler"
@@ -18,20 +19,20 @@ export default function index() {
           </button>
           <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="/#">
-                HOME
+              <a class="nav-link active" aria-current="page" href="/">
+                <p class="nav-text">HOME</p>
               </a>
-              <a class="nav-link" href="/#">
-                ABOUT ME
+              <a class="nav-link" href="/home">
+                <p class="nav-text">ABOUT ME</p>
               </a>
-              <a class="nav-link" href="/#">
-                EXPERIENCE
+              <a class="nav-link" href="/experience">
+                <p class="nav-text">EXPERIENCE</p>
               </a>
-              <a class="nav-link" href="/#">
-                PROJECT
+              <a class="nav-link" href="/projects">
+                <p class="nav-text">PROJECT</p>
               </a>
-              <a class="nav-link" href="/#">
-                CONTACT ME
+              <a class="nav-link" href="/contact">
+                <p class="nav-text">CONTACT ME</p>
               </a>
             </div>
           </div>
